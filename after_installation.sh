3e0182030bb4138c9e97d3ec5f39eabd730cc295
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-23 17:26:07 +0100

    Add files via upload

---
+++ after_installation.sh
@@ -0,0 +1,8 @@
+sudo pacman -Syu pulseaudio fakeroot go make gcc
+mkdir ~/aur
+cd ~/aur
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+echo "yay is now installed. You can install any WM if you want."
