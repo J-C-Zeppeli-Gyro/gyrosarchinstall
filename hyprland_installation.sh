d1b7eac8669c6663ec7d44a483a003ebf85e4449
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-23 17:51:17 +0100

    Add files via upload

--- hyprland_installation.sh
+++ hyprland_installation.sh
@@ -1,3 +1,3 @@
-yay -Syu hyprland dunst kitty thunar rofi xdg-desktop-portal-hyprland qt5-wayland qt6-wayland sddm
+yay -Syu hyprland dunst kitty thunar rofi qt5-wayland qt6-wayland sddm
 sudo systemctl enable sddm
 sudo reboot
