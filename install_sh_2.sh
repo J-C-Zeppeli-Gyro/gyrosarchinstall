1641f45e1d36616ed359920ae59871b0df498122
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-23 17:10:42 +0100

    Add files via upload

--- install_sh_2.sh
+++ install_sh_2.sh
@@ -1,20 +1,7 @@
 pacstrap /mnt base linux linux-firmware sudo grub efibootmgr networkmanager intel-ucode vim
 genfstab -U /mnt >> /mnt/etc/fstab
+clear
+echo "Run install script no. 3!"
+echo "Just type in: ./install_sh_3.sh"
+cp install_sh_3.sh /mnt/ 
 arch-chroot /mnt
-ls -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
-hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP="hu"" >> /etc/vconsole.conf
-systemctl enable NetworkManager
-useradd -m -G wheel -s /bin/bash gyro
-echo "%wheel ALL=(ALL) ALL" >> visudo
-clear
-echo "Set root password below:"
-passwd
-clear
-echo "Set user password below:"
-passwd gyro
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
---
+++ install_sh_3.sh
@@ -0,0 +1,17 @@
+ls -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP="hu"" >> /etc/vconsole.conf
+systemctl enable NetworkManager
+useradd -m -G wheel -s /bin/bash gyro
+echo "%wheel ALL=(ALL) ALL" >> visudo
+clear
+echo "Set root password below:"
+passwd
+clear
+echo "Set user password below:"
+passwd gyro
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
