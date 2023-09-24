71d6868e64870c84c2cafc4945d151c631779996
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-24 18:23:06 +0100

    Add files via upload

--- after_installation.sh
+++ after_installation.sh
@@ -1,7 +1,7 @@
 sudo pacman -Syu fakeroot go make gcc git neofetch base-devel nano neovim openssh wget iwd wireless_tools wpa_supplicant xdg-utils pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber gst-plugins-bad gst-plugins-base gst-plugins-good epiphany
 yay -Syu gst-plugin-openh264
 systemctl --user enable pipewire.service
-systemctl --user enable pipwire-pulse.service
+systemctl --user enable pipewire-pulse.service
 mkdir ~/aur
 cd ~/aur
 git clone https://aur.archlinux.org/yay.git
