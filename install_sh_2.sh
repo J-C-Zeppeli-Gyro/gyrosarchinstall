1809a6a36ccfc668e8708a3e0ea91c18b73bedd4
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-24 11:28:03 +0100

    Add files via upload

--- install_sh_2.sh
+++ install_sh_2.sh
@@ -1,7 +1,10 @@
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+mount /dev/sda2 /mnt
 pacstrap /mnt base linux linux-firmware sudo grub efibootmgr networkmanager intel-ucode vim
 genfstab -U /mnt >> /mnt/etc/fstab
 clear
 echo "Run install script no. 3!"
 echo "Just type in: ./install_sh_3.sh"
-cp * /mnt/ 
+cp *.sh /mnt/ 
 arch-chroot /mnt
