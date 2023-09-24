d3178f21a0e697890dc95cd95aeb32af6ea6e858
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-24 19:24:53 +0100

    Add files via upload

--- hyprland_installation_1.sh
+++ hyprland_installation_1.sh
@@ -1,4 +1,3 @@
 yay -Syu hyprland dunst kitty thunar rofi qt5-wayland qt6-wayland sddm ranger xdg-desktop-portal-hyprland xdg-desktop-portal pw-volume
 sudo systemctl enable sddm
 echo "Restart and run the hyprland 2nd insallation script!"
-#sudo reboot
