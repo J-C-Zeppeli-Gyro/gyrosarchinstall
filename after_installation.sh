e411828af496fd9f680a762da471ddaec5f28804
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-24 16:26:16 +0100

    Add files via upload

--- after_installation.sh
+++ after_installation.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu fakeroot go make gcc git neofetch base-devel nano nvim openssh wget iwd wireless_tools wpa_supplicant xdg-utils pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber
+sudo pacman -Syu fakeroot go make gcc git neofetch base-devel nano neovim openssh wget iwd wireless_tools wpa_supplicant xdg-utils pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber
 sudo systemctl enable pipewire-pulse.service
 mkdir ~/aur
 cd ~/aur
