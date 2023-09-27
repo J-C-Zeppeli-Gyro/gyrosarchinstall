7c1ee8ff214899d4111eb7592b4a8a4e7fef0bc5
Gyro <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-09-27 19:38:59 +0100

    Add files via upload

--- after_installation.sh
+++ after_installation.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu fakeroot go make gcc git neofetch base-devel nano neovim openssh wget iwd wireless_tools xdg-utils pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber gst-plugins-bad gst-plugins-base gst-plugins-good epiphany
+sudo pacman -Syu fakeroot go make gcc git neofetch base-devel nano neovim openssh wget iwd wireless_tools xdg-utils pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber gst-plugins-bad gst-plugins-base gst-plugins-good
 yay -Syu gst-plugin-openh264
 systemctl --user enable pipewire.service
 systemctl --user enable pipewire-pulse.service
--- hyprland_installation_1.sh
+++ hyprland_installation_1.sh
@@ -1,3 +1,3 @@
-yay -Syu hyprland dunst kitty thunar rofi qt5-wayland qt6-wayland sddm ranger xdg-desktop-portal-hyprland xdg-desktop-portal pw-volume
+yay -Syu hyprland dunst kitty thunar rofi qt5-wayland qt6-wayland sddm ranger xdg-desktop-portal-hyprland xdg-desktop-portal pw-volume ungoogled-chromium
 sudo systemctl enable sddm
 echo "Restart and run the hyprland 2nd insallation script!"
