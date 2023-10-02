07ba2cfe1642e984d86fa57eb74e1d4873966f42
Debreceni Norbert György <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-10-02 18:58:17 +0200

    Add files via upload

--- after_installation.sh
+++ after_installation.sh
@@ -1,5 +1,4 @@
-sudo pacman -Syu fakeroot go make gcc git neofetch base-devel nano neovim openssh wget iwd wireless_tools xdg-utils pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber gst-plugins-bad gst-plugins-base gst-plugins-good
-yay -Syu gst-plugin-openh264
+sudo pacman -Syu go git neofetch nano neovim openssh wget iwd wireless_tools xdg-utils pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire libpulse wireplumber gst-plugins-bad gst-plugins-base gst-plugins-good
 systemctl --user enable pipewire.service
 systemctl --user enable pipewire-pulse.service
 mkdir ~/aur
@@ -9,3 +8,4 @@ cd yay
 makepkg -si
 cd
 echo "yay is now installed. You can install any WM if you want."
+yay -Syu gst-plugin-openh264
