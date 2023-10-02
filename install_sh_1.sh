7499c8079343d10b429192562042972b74912ae1
Debreceni Norbert György <74025050+J-C-Zeppeli-Gyro@users.noreply.github.com>
2023-10-02 17:44:00 +0200

    Add files via upload

--- install_sh_1.sh
+++ install_sh_1.sh
@@ -1,5 +1,4 @@
 loadkeys hu
 timedatectl set-timezone CET
-timedatectl
 timedatectl set-ntp true
 echo -e "Now you have to create the partitions with the cfdisk command, create swap!\nsda1 will be /boot, sda2 will be swap, and sda3 will be /\nuse gpt - !!!Note make sure you want to use sda drive for this, or you have to edit the 2. script!!!"
